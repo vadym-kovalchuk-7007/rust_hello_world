c6f680a0a69bc4cff7037d79de6ff98be6d04ded
Vadym Kovalchuk <endnet@ukr.net>
2025-03-24 17:48:28 +0200

    Owning and Borrowing. sep

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused_variables)]
 
+mod references;
+
 use std::ops::Range;
 
 fn count_down(num: u8) {
@@ -32,28 +34,6 @@ fn main() {
     count_down(5);
     println!("{}", color_to_number("green"));
     println!("{}", factorial(5));
-    let mut cereals: [&str; 5] = [
-        "Cookie Crisp",
-        "Cinnamon Toast Crunch",
-        "Frosted Flakes",
-        "Cocoa Puffs",
-        "Captain Crunch",
-    ];
-    let first_two: &[&str] = &cereals[..2];
-    println!("{first_two:?}");
-    let mid_three: &[&str] = &cereals[1..4];
-    println!("{mid_three:?}");
-    let last_thee: &mut [&str] = &mut cereals[2..];
-    last_thee[2] = "Lucky Charms";
-    println!("{cereals:?}");
-    let cookie_crisp: String = String::from(cereals[0]);
-    println!("{cookie_crisp}");
-    let cookie: &str = &cookie_crisp[..6];
-    println!("{cookie}");
-    let cook_puffs: &String = &String::from(cereals[3]);
-    println!("{cook_puffs}");
-    let pufs: &str = &cook_puffs[6..];
-    println!("{pufs}");
 }
 
 fn apply_to_jobs(number: i32, title: &str) {
---
+++ src/references.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut cereals: [&str; 5] = [
+        "Cookie Crisp",
+        "Cinnamon Toast Crunch",
+        "Frosted Flakes",
+        "Cocoa Puffs",
+        "Captain Crunch",
+    ];
+    let first_two: &[&str] = &cereals[..2];
+    println!("{first_two:?}");
+    let mid_three: &[&str] = &cereals[1..4];
+    println!("{mid_three:?}");
+    let last_thee: &mut [&str] = &mut cereals[2..];
+    last_thee[2] = "Lucky Charms";
+    println!("{cereals:?}");
+    let cookie_crisp: String = String::from(cereals[0]);
+    println!("{cookie_crisp}");
+    let cookie: &str = &cookie_crisp[..6];
+    println!("{cookie}");
+    let cook_puffs: &String = &String::from(cereals[3]);
+    println!("{cook_puffs}");
+    let pufs: &str = &cook_puffs[6..];
+    println!("{pufs}");
+}
